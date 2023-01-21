8e701c5d6be135290e905d8d7bf92e6e40ee8fa2
stefanoHTB <danielelucca001@outlook.com>
2023-01-21 18:12:03 -0500

    first commit

---
+++ src/scenes/Navbar/index.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon} from '@heroicons/react/24/solid';
+import Logo from '@/assets/Logo.png'
+import Link from "./Link";
+import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+
+type Props = {
+    selectedPage: SelectedPage;
+    setSelectedPage: (value: SelectedPage) => void;
+}
+
+const Navbar = ({ selectedPage, setSelectedPage}: Props) => {
+
+    const flexBetween = "flex items-center justify-between"
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+    const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false)
+
+
+  return (
+    <nav>
+        <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+
+            <div className={`${flexBetween} mx-auto w-5/6`}>
+
+                {/* left side */}
+                <div className={`${flexBetween} w-full gap-16`}>
+
+                    <img alt="logo" src={Logo} />
+                    
+                   {/* right side */}
+                   {isAboveMediumScreens ? ( 
+                  <div className={`${flexBetween} w-full`}>
+                    {/* inner left */}
+                     <div className={`${flexBetween} gap-8 text-sm`}>
+                        <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                        <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                        <Link page="Our Classes" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                        <Link page="Contact us" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                     </div> 
+                    {/* inner right */}
+                     <div className={`${flexBetween} gap-8`}>
+                        <p>Sign In</p>
+                        <button>Become a Member</button>
+                     </div>                  
+                    </div>) : 
+                    (
+                    <button className="rounded-full bg-secondary-500 p-2"
+                        onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                        <Bars3Icon className="h-6 w-6 text-white" />
+                    </button>
+                    )}
+               </div>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar;
